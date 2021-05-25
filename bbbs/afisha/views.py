787f473948ba5e00ca11d8f583d7d3af90b45d79
Andrew <Andrey18651@gmail.com>
2021-05-25 23:15:10 +0300

    change EventViewSet in afisha/views, add "get_events_by_user" method in it

--- bbbs/afisha/views.py
+++ bbbs/afisha/views.py
@@ -1,12 +1,25 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
+
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from bbbs.afisha.models import Event, EventParticipant
-from bbbs.afisha.serializers import EventParticipantSerializer, EventSerializer
+from bbbs.afisha.serializers import EventSerializer, EventParticipantSerializer
+from bbbs.common.models import Profile
 
 
-class EventList(generics.ListAPIView):
-    queryset = Event.objects.all().order_by('start_at')
-    serializer_class = EventSerializer
+class EventViewSet(viewsets.ViewSet):
+
+    def get_events_by_user(self, request: Request, city_id: int = None):
+        if request.user.id:
+            user_profile = Profile.objects.get(user__id=request.user.id)
+            user_city_id = user_profile.city.id
+            events_queryset = Event.objects.filter(city__id=user_city_id)
+            serializer = EventSerializer(events_queryset, many=True)
+            return Response({'events': serializer.data})
+        events_queryset = Event.objects.filter(city__id=city_id)
+        serializer = EventSerializer(events_queryset, many=True)
+        return Response({'events': serializer.data})
 
 
 class EventParticipantList(generics.ListCreateAPIView, generics.DestroyAPIView):
